5f605b9f7474628702fbe90d6896815348d240d3
GiannBattistutta <148446234+GiannBattistutta@users.noreply.github.com>
2026-05-04 18:01:04 -0300

    Change default port from 10000 to 5050

--- app.py
+++ app.py
@@ -72,5 +72,5 @@ app = create_app()
 if __name__ == "__main__":
     import os
 
-    port = int(os.environ.get("PORT", 10000))
+    port = int(os.environ.get("PORT", 5050))
     app.run(host="0.0.0.0", port=port)
